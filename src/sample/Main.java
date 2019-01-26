464e3bad165d7ca03c8e506551d42bba32ecbe02
Kareem James <kareemjames@MacBook-Pro-5.local>
2019-01-26 16:06:55 -0500

    add scaffold for file menu in player

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,12 +5,19 @@ import javafx.scene.Scene;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
+import java.awt.*;
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+
+        MenuItem open = new MenuItem("Open");
+        Menu file = new Menu("File");
+        MenuBar menu = new MenuBar();
+        
         Player player = new Player("file:///Users/kareemjames/Downloads/videoplayback.mp4");
-        Scene scene = new Scene(player, 720, 510, Color.BLACK);
+        Scene scene = new Scene(player, 720, 535, Color.BLACK);
         primaryStage.setScene(scene);
         primaryStage.show();
 
