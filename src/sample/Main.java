a6dfb710fe3c43acd521c8935f551a7a389439e3
Kareem James <kareemjames@MacBook-Pro-5.local>
2019-01-26 12:50:50 -0500

    add player class to create player window, able to play video from file

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -8,8 +8,8 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Player player = new Player("file://C:/gotg.mp4");
+    public void start(Stage primaryStage) {
+        Player player = new Player("file:///Users/kareemjames/Downloads/videoplayback.mp4");
         Scene scene = new Scene(player, 720, 480, Color.BLACK);
         primaryStage.setScene(scene);
         primaryStage.show();
